3df31e71e1eba4669fb0ce02eb2ab3ac531f3891
yasminGedanken <57451811+yasminGedanken@users.noreply.github.com>
2020-05-14 15:27:41 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -1,43 +1,167 @@
-#include "FamilyTree.hpp"
 #include "doctest.h"
-#include <string.h>
+#include "FamilyTree.hpp"
 
-using namespace family;
+#include <string>
+#include <iostream>
 using namespace std;
+using namespace family;
 
+TEST_CASE("Test for relation method") {
+	Tree T("Yosef");
+	CHECK(T.relation("Yosef") == string("me"));
+	CHECK(T.relation("Yaakov") == string("unrelated"));
+	T.addFather("Yosef", "Yaakov");
+	CHECK(T.relation("Yaakov") == string("father"));
+	CHECK(T.find("father") == string("Yaakov"));
+	CHECK(T.relation("Rachel") == string("unrelated"));
+	T.addMother("Yosef", "Rachel");
+	CHECK(T.relation("Rachel") == string("mother"));
+	CHECK(T.find("mother") == string("Rachel"));
+	CHECK(T.relation("Isaac") == string("unrelated"));
+	T.addFather("Yaakov", "Isaac");
+	CHECK(T.relation("Isaac") == string("grandfather"));
+	CHECK(T.find("grandfather") == string("Isaac"));
+	CHECK(T.relation("Rivka") == string("unrelated"));
+	T.addMother("Yaakov", "Rivka");
+	CHECK(T.relation("Rivka") == string("grandmother"));
+	CHECK(T.find("grandmother") == string("Rivka"));
+	CHECK(T.relation("Shimon") == string("unrelated"));
+	T.addFather("Rachel", "Shimon");
+	CHECK(T.relation("Shimon") == string("grandfather"));
+	CHECK(((T.find("grandfather") == string("Shimon")) || (T.find("grandfather") == string("Isaac")))); // According to BFS
+	T.remove("Shimon");
+	CHECK(T.find("grandfather") == string("Isaac")); // According to BFS
+	CHECK(T.relation("Oshrit") == string("unrelated"));
+	T.addMother("Rachel", "Oshrit");
+	CHECK(T.relation("Oshrit") == string("grandmother"));
+	CHECK(((T.find("grandmother") == string("Oshrit")) || (T.find("grandmother") == string("Rivka")))); // According to BFS
+	T.remove("Oshrit");
+	CHECK(T.find("grandmother") == string("Rivka")); // According to BFS
+	CHECK(T.relation("Avraham") == string("unrelated"));
+	T.addFather("Isaac", "Avraham");
+	CHECK(T.relation("Avraham") == string("great-grandfather"));
+	CHECK(T.relation("Terah") == string("unrelated"));
+	T.addFather("Avraham", "Terah");
+	CHECK(T.relation("Terah") == string("great-great-grandfather"));
+	CHECK(T.relation("Shlomit") == string("unrelated"));
+	T.addMother("Isaac", "Shlomit");
+	CHECK(T.relation("Shlomit") == string("great-grandmother"));
+	CHECK(T.relation("Baruch") == string("unrelated"));
+	T.addFather("Shlomit", "Baruch");
+	CHECK(T.relation("Baruch") == string("great-great-grandfather"));
+	CHECK(((T.find("great-great-grandfather") == string("Baruch")) || (T.find("great-great-grandfather") == string("Terah")))); // According to BFS
+	T.remove("Baruch");
+	CHECK(T.find("great-great-grandfather") == string("Terah")); // According to BFS
 
-TEST_CASE("Test 1 addFather & addMother & relation") {
+}
 
-    Tree T ("Shalom");
-    T.addFather("Shalom", "Aharon").addMother("Shalom", "Yafa");
+TEST_CASE("Test for find method") {
+	Tree T("Yosef");
+	CHECK_THROWS_AS(T.find("father"), std::exception);
+	T.addFather("Yosef", "Yaakov");
+	CHECK_THROWS_AS(T.find("mother"), std::exception);
+	T.addMother("Yosef", "Rachel");
+	CHECK_THROWS_AS(T.find("grandfather"), std::exception);
+	T.addFather("Yaakov", "Isaac");
+	CHECK_THROWS_AS(T.find("grandmother"), std::exception);
+	T.addMother("Yaakov", "Rivka");
+	CHECK_THROWS_AS(T.find("great-grandfather"), std::exception);
+	T.addFather("Rachel", "Shimon");
+	CHECK_THROWS_AS(T.find("great-grandmother"), std::exception);
+	T.addMother("Rachel", "Oshrit");
+	CHECK_THROWS_AS(T.find("great-grandfather"), std::exception);
+	T.addFather("Isaac", "Avraham");
+	CHECK_THROWS_AS(T.find("great-grandmother"), std::exception);
+	T.addMother("Isaac", "Shlomit");
+	CHECK_THROWS_AS(T.find("great-great-grandfather"), std::exception);
+	T.addFather("Avraham", "Terah");
+	CHECK(T.find("me") == string("Yosef"));
+	CHECK(T.find("father") == string("Yaakov"));
+	CHECK(T.find("mother") == string("Rachel"));
+	CHECK(((T.find("grandfather") == string("Shimon")) || (T.find("grandfather") == string("Isaac"))));
+	CHECK(((T.find("grandmother") == string("Oshrit")) || (T.find("grandmother") == string("Rivka"))));
+	CHECK(T.find("great-grandfather") == string("Avraham"));
+	CHECK(T.find("great-grandmother") == string("Shlomit"));
+	T.addFather("Shlomit", "Avner");
+	CHECK(((T.find("great-great-grandfather") == string("Avner")) || (T.find("great-great-grandfather") == string("Terah"))));
+	T.remove("Avner");
+	CHECK(T.find("great-great-grandfather") == string("Terah"));
+}
 
-    CHECK(T.relation("Shalom") == string("me"));
-    CHECK(T.relation("Aharon") == string("father"));
-    CHECK(T.relation("Yafa") == string("mother"));
+TEST_CASE("Test for remove method") {
+	Tree T("Yosef");
+	T.addFather("Yosef", "Yaakov");
+	T.addMother("Yosef", "Rachel");
+	T.addFather("Yaakov", "Isaac");
+	T.addMother("Yaakov", "Rivka");
+	T.addFather("Isaac", "Avraham");
+	T.addFather("Avraham", "Terah");
+	T.addMother("Isaac", "Dana");
+	T.addFather("Rachel", "Nissim");
+	T.addMother("Rachel", "Efrat");
+	CHECK(T.relation("Avraham") == string("great-grandfather"));
+	CHECK(T.relation("Terah") == string("great-great-grandfather"));
+	T.remove("Avraham");
+	CHECK(T.relation("Avraham") == string("unrelated"));
+	CHECK(T.relation("Terah") == string("unrelated"));
+	CHECK(T.relation("Yaakov") == string("father"));
+	CHECK(T.relation("Isaac") == string("grandfather"));
+	CHECK(T.relation("Rivka") == string("grandmother"));
+	CHECK(T.relation("Efrat") == string("grandmother"));
+	CHECK(((T.find("grandmother") == string("Efrat")) || (T.find("grandmother") == string("Rivka"))));
+	CHECK(((T.find("grandfather") == string("Nissim")) || (T.find("grandfather") == string("Isaac"))));
+	CHECK(T.relation("Rachel") == string("mother"));
+	T.remove("Rachel");
+	CHECK(T.relation("Nissim") == string("unrelated"));
+	CHECK(T.relation("Efrat") == string("unrelated"));
+	CHECK(T.relation("Rachel") == string("unrelated"));
+	CHECK(T.find("grandmother") == string("Rivka"));
+	CHECK(T.find("grandfather") == string("Isaac"));
+	T.remove("Yaakov");
+	CHECK(T.relation("Rivka") == string("unrelated"));
+	CHECK(T.relation("Isaac") == string("unrelated"));
+	CHECK(T.relation("Yaakov") == string("unrelated"));
+}
 
-    CHECK_THROWS(T.addFather("Shalom", "Yosef")); //Shalom already has a father
-    CHECK_THROWS(T.addMother("Shalom", "Meri"));
-
-    T.addMother("Yafa", "Ahuva").addMother("Ahuva", "Miriam");
-
-    CHECK(T.relation("Ahuva") == string("grandmother"));
-    CHECK(T.relation("Miriam") == string("great-grandmother"));
-    CHECK_THROWS(T.addMother("Yafa", "Michal"));
-    CHECK_THROWS(T.addMother("Ahuva", "Michal"));
-    CHECK_THROWS(T.addFather("Sason", "Yaakov")); //Sason is not on the tree
-
-    T.addFather("Yafa", "Avi").addFather("Aharon", "Yigal").addMother("Aharon", "Nira");
-
-    CHECK(T.relation("Avi") == string("grandfather"));
-    CHECK(T.relation("Yigal") == string("grandfather"));
-    CHECK(T.relation("Nira") == string("grandmother"));
-    CHECK_THROWS(T.addFather("Yafa", "Yishai"));
-    CHECK_THROWS(T.addMother("Aharon", "Tzila"));
-
-    //check case of searching name that doesn't exist on the tree 
-    CHECK(T.relation("Hila") == string("unrelated"));
-    CHECK(T.relation("Ben") == string("unrelated"));
-    CHECK(T.relation("Eva") == string("unrelated"));
-    CHECK(T.relation("Shimon") == string("unrelated"));
-    CHECK(T.relation("Yossi") == string("unrelated"));
+TEST_CASE("Test for exceptions") {
+	Tree T("Yosef");
+	T.addFather("Yosef", "Yaakov");
+	T.addMother("Yosef", "Rachel");
+	T.addFather("Yaakov", "Isaac");
+	T.addMother("Yaakov", "Rivka");
+	T.addFather("Isaac", "Avraham");
+	T.addFather("Avraham", "Terah");
+	CHECK_THROWS_AS(T.addFather("Yosef", "Avner"), std::exception);
+	CHECK_THROWS_AS(T.addMother("Yosef", "Bella"), std::exception);
+	CHECK_THROWS_AS(T.addFather("Yaakov", ""), std::exception);
+	CHECK_THROWS_AS(T.addMother("", "Dana"), std::exception);
+	CHECK_THROWS_AS(T.addFather("", ""), std::exception);
+	CHECK_THROWS_AS(T.addMother("", ""), std::exception);
+	CHECK_THROWS_AS(T.addFather("Moshe", "David"), std::exception);
+	CHECK_THROWS_AS(T.addMother("Dan", "Dana"), std::exception);
+	CHECK_THROWS_AS(T.find(""), std::exception);
+	CHECK_THROWS_AS(T.find("great-grandmother"), std::exception);
+	CHECK_THROWS_AS(T.find("great-great-grandmother"), std::exception);
+	CHECK_THROWS_AS(T.remove(""), std::exception);
+	CHECK_THROWS_AS(T.remove("David"), std::exception);
+	CHECK_THROWS_AS(T.remove("Shlomo"), std::exception);
+	CHECK_THROWS_AS(T.remove("Isaschar"), std::exception);
+	CHECK_THROWS_AS(T.remove("Ezra"), std::exception);
+	CHECK_THROWS_AS(T.remove("Ted"), std::exception);
+	T.remove("Isaac");
+	CHECK_THROWS_AS(T.find("grandfather"), std::exception);
+	CHECK_THROWS_AS(T.find("great-grandfather"), std::exception);
+	CHECK_THROWS_AS(T.find("uncle"), std::exception);
+	CHECK_THROWS_AS(T.find("grand-grandfather"), std::exception);
+	CHECK_THROWS_AS(T.find("great-grandfather"), std::exception);
+	CHECK_THROWS_AS(T.find("great-greatfather"), std::exception);
+	CHECK_THROWS_AS(T.find("great-grandmother"), std::exception);
+	CHECK_THROWS_AS(T.find("dad"), std::exception);
+	CHECK_THROWS_AS(T.find("daddy"), std::exception);
+	CHECK_THROWS_AS(T.find("mom"), std::exception);
+	CHECK_THROWS_AS(T.find("mommy"), std::exception);
+	CHECK_THROWS_AS(T.find("grandpa"), std::exception);
+	CHECK_THROWS_AS(T.find("grandma"), std::exception);
+	CHECK_THROWS_AS(T.find(""), std::exception);
+	CHECK_THROWS_AS(T.remove("Isaac"), std::exception); // Already done
 }
